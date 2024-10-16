d8697db70e9f0949b24b9561f68d362a1e5869a1
Sebastian Hädrich <sebastian.haedrich@mailbox.org>
2024-10-16 14:10:26 +0200

    refactor: :truck: Rename config and service provider to new name

--- src/WritingStyleServiceProvider.php
+++ src/WritingStyleServiceProvider.php
@@ -5,19 +5,19 @@ namespace LaravelWorkingGroup\WritingStyle;
 
 use Illuminate\Support\ServiceProvider;
 
-class TitlesServiceProvider extends ServiceProvider
+class WritingStyleServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
         $this->mergeConfigFrom(
-            __DIR__.'/../config/titles.php', 'titles'
+            __DIR__.'/../config/writing-style.php', 'writing-style'
         );
     }
 
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/../config/titles.php' => config_path('titles.php'),
+            __DIR__.'/../config/writing-style.php' => config_path('writing-style.php'),
         ]);
     }
 }
